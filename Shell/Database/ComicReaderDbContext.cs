1adbb0c0d0c6c882b32f788e90ae0fc7fd4f4e54
Satiriques <bad_ghost2005@hotmail.com>
2019-08-24 14:38:18 -0400

    cleaned up db context

--- Shell/Database/ComicReaderDbContext.cs
+++ Shell/Database/ComicReaderDbContext.cs
@@ -13,32 +13,5 @@ namespace ComicReader.Net.Shell.Database
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Character> Characters { get; set; }
         public DbSet<Thumbnail> Thumbnails { get; set; }
-
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Book>()
-        //        .HasMany(b => b.Tags)
-        //        .WithRequired(t => t.Book)
-        //        .HasForeignKey(t => t.BookId);
-
-        //    //modelBuilder.Entity<Book>()
-        //    //    .HasMany(b => b.Characters)
-        //    //    .WithRequired(c => c.Book)
-        //    //    .HasForeignKey(c => c.BookId);
-
-        //    //modelBuilder.Entity<Book>()
-        //    //    .HasMany(b => b.Thumbnails)
-        //    //    .WithRequired(t => t.Book)
-        //    //    .HasForeignKey(t => t.BookId);
-
-        //    //modelBuilder.Entity<Book>()
-        //    //    .HasKey(b => new { b.Id, b.Path });
-
-        //    //modelBuilder.Entity<Tag>()
-        //    //    .HasKey(t => new { t.TagId, t.Name });
-
-        //    //modelBuilder.Entity<Thumbnail>()
-        //    //    .HasKey(t => new { t.ThumbnailId, t.Path });
-        //}
     }
 }
