3950119c0e50ecbd48c5ba0824a4447350b06b44
Satiriques <bad_ghost2005@hotmail.com>
2019-08-29 20:20:40 -0400

    added first unit test :D testing out moq library added ncrunch config

---
+++ ShellTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShellTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShellTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a848d511-c738-4641-90be-23621161d775")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ShellTests/ViewModelTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ComicReader.Net.ApplicationMenu.Interfaces;
+using ComicReader.Net.CenterGrid.Interfaces;
+using ComicReader.Net.Shell.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ShellTests
+{
+    [TestClass]
+    public class ViewModelTests
+    {
+        [TestMethod]
+        public void MainViewModelTest()
+        {
+            var fileMenuViewModel = new Mock<IFileMenuViewModel>();
+            var centerGridViewModel = new Mock<ICenterGridViewModel>();
+            var viewModel = new MainViewModel(fileMenuViewModel.Object, centerGridViewModel.Object);
+
+            Assert.IsNotNull(viewModel.CenterGridViewModel);
+            Assert.IsNotNull(viewModel.FileMenuViewModel);
+        }
+    }
+}
