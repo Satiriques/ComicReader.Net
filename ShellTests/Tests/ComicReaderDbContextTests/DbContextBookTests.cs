ff931099b90c194ddd3cb859431e99002cbe9c86
Satiriques <bad_ghost2005@hotmail.com>
2019-09-02 15:33:56 -0400

    added book test

--- ShellTests/Tests/ComicReaderDbContextTests/DbContextBookTests.cs
+++ ShellTests/Tests/ComicReaderDbContextTests/DbContextBookTests.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ComicReader.Net.Common.Models;
 
 namespace ShellTests.Tests.ComicReaderDbContextTests
 {
-    internal class DbContextBookTests
+    public class DbContextBookTests : DbContextTestBase
     {
+        [Test]
+        public async Task AddBookTest()
+        {
+            var dbHelper = DbHelperCollection.Take();
+            var db = dbHelper.Db;
+
+            var book = new Book() { Path = @"c:\testFile.txt" };
+            db.Books.Add(book);
+            await db.SaveChangesAsync();
+
+            var books = db.Books.ToList();
+            Assert.IsNotNull(books);
+            CollectionAssert.IsNotEmpty(books);
+            Assert.AreEqual(book.Path, books[0].Path);
+
+            Cleanup(dbHelper);
+        }
     }
 }
