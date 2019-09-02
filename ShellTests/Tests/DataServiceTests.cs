17d704b54866836ff4e4430041ecef47a42c399b
Satiriques <bad_ghost2005@hotmail.com>
2019-09-02 17:41:28 -0400

    added file service test in progress

--- ShellTests/Tests/DataServiceTests.cs
+++ ShellTests/Tests/DataServiceTests.cs
@@ -3,47 +3,49 @@ using ComicReader.Net.Shell.Database;
 using ComicReader.Net.Shell.Services;
 using Moq;
 using NUnit.Framework;
+using ShellTests.Tests.ComicReaderDbContextTests;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ShellTests.Tests
 {
-    public class DataServiceTests
+    public class DataServiceTests : DbContextTestBase
     {
-        private DataService _dataService;
-
-        [SetUp]
-        public void SetDataService()
-        {
-            string dbFile = @$".\test_{Thread.CurrentThread.ManagedThreadId}.sdf";
-            var func = new Func<ComicReaderDbContext>(() =>
-            new ComicReaderDbContext($"Data Source={dbFile}", new DropCreateDatabaseAlways<ComicReaderDbContext>()));
-            _dataService = new DataService(func, new Mock<IFileService>().Object, new Mock<IZipService>().Object, new Mock<IParserService>().Object);
-        }
-
         //[Test]
         public async Task AddBookTest()
         {
-            await _dataService.AddBooksAsync(new string[] { @"c:\randomFile.txt" });
+            var dbHelper = DbHelperCollection.Take();
+            var db = dbHelper.Db;
+            var dataService = new DataService(new Func<ComicReaderDbContext>(() => db), new FileService(), new ZipService(), new ParserService());
+
+            var path = Path.Combine(Assembly.GetExecutingAssembly().Location, "..", "Data", "TestBook.zip");
+
+            Assert.IsTrue(File.Exists(path));
+            await dataService.AddBooksAsync(new string[] { path });
+
+            var books = await dataService.GetAllBooksAsync();
+
+            Cleanup(dbHelper);
         }
 
         //[Test]
         public async Task GetAllBooksAsync()
         {
-            var books = await _dataService.GetAllBooksAsync();
-            CollectionAssert.IsEmpty(books);
+            //var books = await _dataService.GetAllBooksAsync();
+            //CollectionAssert.IsEmpty(books);
 
-            await _dataService.AddBooksAsync(new string[] { @"c:\randomFile.txt", @"c:\randomFile2.txt" });
+            //await _dataService.AddBooksAsync(new string[] { @"c:\randomFile.txt", @"c:\randomFile2.txt" });
 
-            books = await _dataService.GetAllBooksAsync();
-            CollectionAssert.AllItemsAreNotNull(books);
-            Assert.AreEqual(2, books.Count);
+            //books = await _dataService.GetAllBooksAsync();
+            //CollectionAssert.AllItemsAreNotNull(books);
+            //Assert.AreEqual(2, books.Count);
         }
 
         [Test]
